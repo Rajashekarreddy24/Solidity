0c04f37a9bb130378a8133da17ee4c7aea765a20
Rajashekarreddy24 <101244107+Rajashekarreddy24@users.noreply.github.com>
2022-05-18 15:27:33 +0530

    Add files via upload

---
+++ Constructor.sol
@@ -0,0 +1,20 @@
+// //SPDX-Licence-Identifier : UNLICENSED
+
+// pragma solidity >=0.8.7;
+// contract test
+// {
+//     uint public count;
+//     constructor(uint new_count)
+//     {
+//         count = new_count;
+//     }
+// }
+
+
+// ///////// integer range //////
+
+// pragma solidity>=0.8.7;
+// contract range
+// {
+//     uint16 public count = 255;
+// }
