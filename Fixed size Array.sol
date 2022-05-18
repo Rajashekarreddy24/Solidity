01748ec622a274ce22b76e71da5b9f9efbbfed69
Rajashekarreddy24 <101244107+Rajashekarreddy24@users.noreply.github.com>
2022-05-18 15:28:37 +0530

    Add files via upload

---
+++ Fixed size Array.sol
@@ -0,0 +1,43 @@
+// //SPDX-Licence-Identifier : UNLICENSED
+
+
+// pragma solidity >=0.8.7;
+// contract array
+// {
+//     uint[5] public arr=[10,20,30,40,50];
+//     function setter(uint index,uint value) public
+//     {
+//         arr[index]=value;
+//     }
+//     function length() public view returns(uint)
+//     {
+//         return arr.length;
+//     }
+
+// }
+// */
+
+// /*
+// pragma solidity>= 0.8.7;
+
+// contract test 
+// {
+//     uint a =10;
+
+// }
+// */
+// /*
+// pragma solidity >=0.8.7;
+// contract test 
+// {
+//     uint[4] public arr=[10,20,30,40] ;
+//     function setter(uint index, uint value) public 
+//     {
+//         arr[index]= value;
+//     }
+//     function length() public view returns(uint)
+//     {
+//         return arr.length;
+//     }
+    
+// }
