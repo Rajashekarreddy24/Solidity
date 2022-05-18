937b30ea0d58d2552c58e1d773ae6a972b63f1cf
Rajashekarreddy24 <101244107+Rajashekarreddy24@users.noreply.github.com>
2022-05-18 15:27:53 +0530

    Add files via upload

---
+++ Dynamic size array.sol
@@ -0,0 +1,21 @@
+// //SPDX-Licence-Identifier : UNLICENSED
+
+
+// pragma solidity >=0.8.7;
+// contract array
+// {
+//     uint[] public arr;
+//     function pushElement(uint item) public
+//     {
+//         arr.push(item);
+//     }
+//     function len() public view returns(uint) 
+//     {
+//         return arr.length;
+//     }
+//     function popElement() public
+//     {
+//         arr.pop();
+//     }
+
+// }
