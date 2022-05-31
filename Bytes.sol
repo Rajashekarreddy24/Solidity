83e56611395d5ee621aa3029df81e21dad2d8050
Rajashekarreddy24 <101244107+Rajashekarreddy24@users.noreply.github.com>
2022-05-31 11:18:00 +0530

    Add files via upload

---
+++ Bytes.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier:MIT
+
+
+pragma solidity ^0.8.0;
+
+contract test{
+
+    bytes public b;
+    bytes2 public b2;
+    bytes4 public b4;
+    bytes16 public b16;
+    bytes32 public b32;
+
+    constructor (){
+
+        b="abc";
+        b2="ab";
+        b4="abc";
+        b16="abc";
+        b32="abc";
+        
+    }
+}
---
+++ Test.sol
@@ -0,0 +1,20 @@
+
+// pragma solidity ^0.5.0;
+
+// contract test{
+
+//     //string[] arr;
+//     // function list() public {
+//     //     arr.push("alndb");
+//     //     arr.push("123jsbc");
+//     // }
+
+//     function Len(string memory str) public pure returns(uint){
+//        return bytes(str).length ;
+//     }
+// }
+
+
+
+
+
